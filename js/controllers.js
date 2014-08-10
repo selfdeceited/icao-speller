179852d5db647be7772e4a7476ec54d2b2af572e
Tony Ryzhikov <Tony Ryzhikov@localhost>
2014-08-10 10:05:00 +0400

    Removed kitten, changed json to js to prevent mime-type errors on deploy.

--- js/controllers.js
+++ js/controllers.js
@@ -6,7 +6,7 @@ var controllers = angular.module('ICAOControllers', []);
 controllers.controller('ICAOStatsController', ['$scope','$http', function($scope, $http) {
 
         var alphabet = {};
-        $http.get('json/icaoAlphabet.json').success(function(data) {
+        $http.get('json/icaoAlphabet.js').success(function(data) {
             alphabet = setStats(data);
         });
 
@@ -20,7 +20,6 @@ controllers.controller('ICAOStatsController', ['$scope','$http', function($scope
                     "name": letterData.name,
                     "value": letterData.value,
                     "russian": letterData.russian,
-                    "imageUrl": letterData.imageUrl,
                     "id": STATIC_ID
                 };
 
@@ -52,14 +51,3 @@ function findDataForSymbol (alphabet, currentSymbol){
     return {"name": currentSymbol, "value": currentSymbol, "russian": currentSymbol};
 }
 
-function setStats(data){
-    for(var i in data){
-        data[i].imageUrl = "http://placekitten.com/" + getRandomInt(70, 100) + "/" + getRandomInt(25, 50);
-    }
-    return data;
-}
-
-
-function getRandomInt(min, max){
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
