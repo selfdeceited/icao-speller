baa4f163c289574e6574410a52c4adf3717b9443
Tony Ryzhikov <selfdeceited@gmail.com>
2015-05-04 12:38:13 +0300

    Updated stress on en and ru versions, added Chuck Norris jokes, fixed other symbols representation, refactored a bit.

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,12 @@
 angular.module('ICAOControllers', ['ngSanitize'])
 .controller('ICAOStatsController', ['$scope','$http', function($scope, $http) {
-        $scope.text = "One morning, when Gregor Samsa woke from troubled";
+        $http.get('http://api.icndb.com/jokes/random').
+          success(function(data, status, headers, config) {
+            $scope.text =  data.value.joke;
+          }).
+          error(function(data, status, headers, config) {
+            $scope.text =  "One morning, when Gregor Samsa woke from troubled dream";
+          });
         $scope.lang = "value";
 
         $scope.toggleLang = function(lang){
@@ -21,10 +27,15 @@ angular.module('ICAOControllers', ['ngSanitize'])
   return function(input, lang, alphabet) {
     var result = [];
     angular.forEach(input, function(symbol) {
+      var spelledCorrectly = false;
       for (var i = 0; i < alphabet.length; i++) {
         if(alphabet[i].name === symbol.toUpperCase()){
             this.push('<span>' + alphabet[i][lang] + '</span>');
+            spelledCorrectly = true;
         }
+    }
+    if(!spelledCorrectly){
+      this.push('<span>' + symbol + '</span>');
     }
     }, result);
     return result.join(' ');
